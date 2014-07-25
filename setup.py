9144cf6dd81242de1ae4fbf187715b22c04977b8
stonebig <stonebig..>
2014-07-25 19:12:00 +0200

    commit to build a first working python package
    all the work to make it work with a winpython distribution

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Julia/Python bridge with IPython support.
+"""
+
+#from setuptools import setup
+from distutils.core import setup
+
+setup(name='julia',
+      version='0.1.1',
+      description=__doc__,
+      author='The Julia and IPython development teams.',
+      author_email='julia@julialang.org',
+      url='http://julialang.org',
+      packages=['julia'],
+     )
